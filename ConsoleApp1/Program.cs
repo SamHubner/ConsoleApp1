30ea3f0fbd6aabb96ca1b55bf24b2a596c42224d
Sam Hubner <K038639@eltham-college.org.uk>
2020-09-07 11:29:52 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            string s1;
+            int x;
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("enter a number");
+
+            s1 = Console.ReadLine();
+
+            num = Convert.ToInt32(s1);
+
+            if (num > 255)
+            {
+                Console.WriteLine("number is too high");
+            } else
+            {
+                x = (num % 16);
+                if (x = 10)
+                { }
+            }
+            
+        
+        }
+    }
+}
